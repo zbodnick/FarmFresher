a8f1fa0fd9847c92cd398d3f494058ad1bdde853
Jake Harris <jakejh@gwu.edu>
2020-04-22 14:54:06 -0400

    Update info

--- teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/updateinfo.php
+++ teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/updateinfo.php
@@ -3,18 +3,18 @@
     session_start();
     $page_title = 'GWU Advising System';
     $error_msg = "";
- 
+
      require_once('connectvars.php');
      require_once('appvars.php');
      require_once('header.php');
      require_once('navmenu.php');
-  
+
      echo '<h4>Personal Information: </h4>';
-     
+
   $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-          
-  $sql = "UPDATE personalinfo SET ftname = '$_POST[fname]', ltname = '$_POST[lname]', dob = '$_POST[dob]', address = '$_POST[address]', cell = '$_POST[cell]' WHERE universid = $_POST[id]";
-       
+
+  $sql = "UPDATE personalinfo SET ftname = '$_POST[fname]', ltname = '$_POST[lname]', address = '$_POST[address]', WHERE universid = $_POST[id]";
+
        if($dbc->query($sql) == TRUE)
        {
          header("refresh:1; url=changeinfo.php");
