36fa4f4cbf554a25bbe8fb2c087f08ecc21d4452
Jake Harris <jakejh@gwu.edu>
2020-04-22 14:35:41 -0400

    Personal Info

--- teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/changeinfo.php
+++ teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/changeinfo.php
@@ -28,21 +28,58 @@
           $result = mysqli_query($dbc, $query);
 
           if(mysqli_num_rows($result) == 0){
-            echo '<body>No personal info found<br/></body>';
-            $ftname = " ";
-            $ltname = " ";
-            $dob = " ";
-            $address = " ";
-            $cell = " ";
+            if(mysqli_num_rows(mysqli_query($dbc, "select fname from student where u_id = $id")) > 0){
+              $ftname = $row["fname"];
+              $ltname = $row["lname"];
+              //$dob = $row["dob"];
+              $address = $row["addr"];
+              //$cell = $row["cell"];
 
-            $dbc->query('SET foreign_key_checks = 0');
-            $val = "INSERT INTO personalinfo VALUES ($id, 'X', 'X', '1900-01-01', 'X', 0);";
-            if($dbc->query($val) == FALSE){
-              echo "$dbc->error";
+              //DISPLAYING USER'S PERSONAL INFO
+              echo "<form action= updateinfo.php method = post> <br>";
+              echo "First Name: "."<input type = text  name = fname value = '".$ftname."'> <br>";
+              echo "Last Name: "."<input type = text  name = lname value = '".$ltname."'> <br>";
+              //echo "Date of Birth: "."<input type = text  name = dob value = '".$dob."'> <br>";
+              echo "Address: "."<input type = text  name = address value = '".$address."'> <br>";
+              //echo "Cellphone: "."<input type = text  name = cell value = '".$cell."'> <br>";
+              echo "<input type = hidden name = id value ='".$row['universid']."'>";
+              echo "<input type = submit>";
+              echo "</form>";
+              echo '<br>';
+            }elseif(mysqli_num_rows(mysqli_query($dbc, "select fname from faculty where u_id = $id")) > 0){
+              $ftname = $row["fname"];
+              $ltname = $row["lname"];
+              //$dob = $row["dob"];
+              $address = $row["addr"];
+              //$cell = $row["cell"];
+
+              //DISPLAYING USER'S PERSONAL INFO
+              echo "<form action= updateinfo.php method = post> <br>";
+              echo "First Name: "."<input type = text  name = fname value = '".$ftname."'> <br>";
+              echo "Last Name: "."<input type = text  name = lname value = '".$ltname."'> <br>";
+              //echo "Date of Birth: "."<input type = text  name = dob value = '".$dob."'> <br>";
+              echo "Address: "."<input type = text  name = address value = '".$address."'> <br>";
+              //echo "Cellphone: "."<input type = text  name = cell value = '".$cell."'> <br>";
+              echo "<input type = hidden name = id value ='".$row['universid']."'>";
+              echo "<input type = submit>";
+              echo "</form>";
+              echo '<br>';
+            }else{
+              echo '<body>No personal info found<br/></body>';
+              $ftname = " ";
+              $ltname = " ";
+              $dob = " ";
+              $address = " ";
+              $cell = " ";
+
+              $dbc->query('SET foreign_key_checks = 0');
+              $val = "INSERT INTO personalinfo VALUES ($id, 'X', 'X', '1900-01-01', 'X', 0);";
+              if($dbc->query($val) == FALSE){
+                echo "$dbc->error";
+              }
+
+              header("refresh:1; url=changeinfo.php");
             }
-
-            header("refresh:1; url=changeinfo.php");
-
           }else{
             while($row = mysqli_fetch_array($result))
               {
