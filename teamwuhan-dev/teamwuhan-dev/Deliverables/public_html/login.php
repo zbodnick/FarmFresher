1ffdf9d60fe093023eced6ef3498e4dd9a43163f
Jake Harris <jakejh@gwu.edu>
2020-04-22 14:01:59 -0400

    Make post a int

--- teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/login.php
+++ teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/login.php
@@ -15,7 +15,7 @@
       $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
       // Grab the user-entered log-in data
-      $user_username = mysqli_real_escape_string($dbc, trim($_POST['username']));
+      $user_username = ($_POST['username']);
       $user_password = mysqli_real_escape_string($dbc, trim($_POST['password']));
 
 
