36de263ff0ba2b11839f6b3e4af35d4c31f70239
Ubuntu <ubuntu@ip-172-31-86-174.ec2.internal>
2020-04-25 21:10:07 +0000

    minor changes to sql

--- Farm-Fresher/src/php/connectvars.php
+++ Farm-Fresher/src/php/connectvars.php
@@ -1,6 +1,10 @@
 <?php
-  define('DB_HOST', 'localhost');
+  /*define('DB_HOST', 'localhost');
   define('DB_USER', 'zbodnick');
   define('DB_PASSWORD', 'FarmFr3$h');
-  define('DB_NAME', 'FarmFresher');
+  define('DB_NAME', 'FarmFresher');*/
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'ubuntu');
+  define('DB_PASSWORD', 'seas');
+  define('DB_NAME', 'ubuntu');
 ?>
