242fbe1d0170f4bcf81e6cb9a419a4f833ffe651
Jake Harris <jakejh@gwu.edu>
2020-04-25 22:47:18 -0400

    Fix

--- teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/enrollment.php
+++ teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/enrollment.php
@@ -65,7 +65,7 @@
       $numberOfFs = $numberOfFs->fetch_assoc();
       $resultF= $numberOfFs['totalF'];
 
-      $query2 = "select sum(A.credits) from (select DISTINCT u_id, semester, year, grade, title, credits from courses_taken join schedule join catalog WHERE u_id = " . $id . " and catalog.c_id = courses_taken.crn) as A;";
+      $query2 = "select sum(A.credits) credits from (select DISTINCT u_id, semester, year, grade, title, credits from courses_taken join schedule join catalog WHERE u_id = " . $id . " and catalog.c_id = courses_taken.crn) as A;";
       $chours = mysqli_query($dbc, $query2);
       $chours = $chours->fetch_assoc();
       $totalhours= $chours['credits'] + 0.00;
