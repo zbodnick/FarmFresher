36e2599ee22385e91e1399193280297effea3dee
Jake Harris <jakejh@gwu.edu>
2020-04-26 18:38:05 -0400

    Apply to grad should be good

--- teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/applygraduate.php
+++ teamwuhan-dev/teamwuhan-dev/Deliverables/public_html/applygraduate.php
@@ -9,37 +9,35 @@
   require_once('navmenu.php');
  	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
   $id = $_SESSION['id'];
-
-
-  $queryA = "SELECT SUM(CASE grade WHEN 'A' THEN 1 ELSE 0 END) totalA FROM transcript WHERE univerid = $id;";
+  $queryA = "SELECT SUM(CASE grade WHEN 'A' THEN 1 ELSE 0 END) totalA FROM courses_taken WHERE u_id = $id;";
   $numberOfAs = mysqli_query($dbc, $queryA);
   $numberOfAs = $numberOfAs->fetch_assoc();
   $resultA= $numberOfAs['totalA'];
 
-  $queryB = "SELECT SUM(CASE grade WHEN 'B' THEN 1 ELSE 0 END) totalB FROM transcript WHERE univerid = $id;";
+  $queryB = "SELECT SUM(CASE grade WHEN 'B' THEN 1 ELSE 0 END) totalB FROM courses_taken WHERE u_id = $id;";
   $numberOfBs = mysqli_query($dbc, $queryB);
   $numberOfBs = $numberOfBs->fetch_assoc();
   $resultB= $numberOfBs['totalB'];
 
-  $queryC = "SELECT SUM(CASE grade WHEN 'C' THEN 1 ELSE 0 END) totalC FROM transcript WHERE univerid = $id;";
+  $queryC = "SELECT SUM(CASE grade WHEN 'C' THEN 1 ELSE 0 END) totalC FROM courses_taken WHERE u_id = $id;";
   $numberOfCs = mysqli_query($dbc, $queryC);
   $numberOfCs = $numberOfCs->fetch_assoc();
   $resultC= $numberOfCs['totalC'];
 
-  $queryD = "SELECT SUM(CASE grade WHEN 'D' THEN 1 ELSE 0 END) totalD FROM transcript WHERE univerid = $id;";
+  $queryD = "SELECT SUM(CASE grade WHEN 'D' THEN 1 ELSE 0 END) totalD FROM courses_taken WHERE u_id = $id;";
   $numberOfDs = mysqli_query($dbc, $queryD);
   $numberOfDs = $numberOfDs->fetch_assoc();
   $resultD= $numberOfDs['totalD'];
 
-  $queryF = "SELECT SUM(CASE grade WHEN 'F' THEN 1 ELSE 0 END) totalF FROM transcript WHERE univerid = $id;";
+  $queryF = "SELECT SUM(CASE grade WHEN 'F' THEN 1 ELSE 0 END) totalF FROM courses_taken WHERE u_id = $id;";
   $numberOfFs = mysqli_query($dbc, $queryF);
   $numberOfFs = $numberOfFs->fetch_assoc();
   $resultF= $numberOfFs['totalF'];
 
-  $query2 = "SELECT SUM(chours) cHOURS FROM transcript WHERE univerid = $id;";
+  $query2 = "select sum(A.credits) credits from (select DISTINCT u_id, semester, year, grade, title, credits from courses_taken join schedule join catalog WHERE u_id = " . $id . " and catalog.c_id = courses_taken.crn) as A;";
   $chours = mysqli_query($dbc, $query2);
   $chours = $chours->fetch_assoc();
-  $totalhours= $chours['cHOURS'];
+  $totalhours= $chours['credits'] + 0.00;
 
   $approvedreturn = "SELECT COUNT(cid) approvedreturn FROM formone WHERE universityid = $id AND cid = 'APPROVED';";
   $formone = mysqli_query($dbc, $approvedreturn);
@@ -62,7 +60,8 @@
    $numBadGrades = numBadGrades($resultC, $resultD, $resultF);
 
 
-  $query3 = "SELECT SUM(chours) choursCSCI FROM transcript WHERE univerid = $id AND crseid LIKE 'CSCI%';";
+  //$query3 = "SELECT SUM(chours) choursCSCI FROM transcript WHERE univerid = $id AND crseid LIKE 'CSCI%';";
+  $query3 = "select sum(A.credits) credits from (select DISTINCT u_id, semester, year, grade, title, credits from courses_taken join schedule join catalog WHERE u_id = " . $id . " and catalog.c_id = courses_taken.crn) as A;";
   $choursCSCI = mysqli_query($dbc, $query3);
   $choursCSCI = $choursCSCI->fetch_assoc();
   $choursCSCItotal = $choursCSCI['choursCSCI'];
