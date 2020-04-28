5edf253c1c02a36f5326f70c5a0df1c2f93df922
Ubuntu <ubuntu@ip-172-31-86-174.ec2.internal>
2020-04-28 00:40:05 +0000

    application view accept/reject fn and review form finalized

--- Farm-Fresher/src/review_form.php
+++ Farm-Fresher/src/review_form.php
@@ -11,71 +11,40 @@
 </head>
 
 <?php
-
-	$error_msg = "";
-	
-	// echo '<hr />';	
-	// echo '<div class="nav"><button id="btn" onclick="window.location.href = \'index.php\';">Home Page</button></div>';
-	// echo '<hr />';
-
 	if (empty($_SESSION['id'])) {
 		header("Location: login.php");
-	}
-	
-	include ('php/connectvars.php');		
-	
-	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-  echo '<hr />';
-  echo '<div class="nav"><button id="btn" onclick="window.location.href = \'reviewer_portal.php\';">Back</button></div>';
-  echo '<hr />';
-
-  $degree = $greMath = $greEnglish = $first = $last = $appid = "";
-  if (isset($_GET['degree_type'])) {
-    $degree = $_GET['degree_type'];
   }
-  if (isset($_GET['GRE_ScoreVerbal'])) {
-    $greMath = $_GET['GRE_ScoreVerbal'];
-  }
-  if (isset($_GET['GRE_ScoreQuantitative'])) {
-    $greEnglish = $_GET['GRE_ScoreQuantitative'];
-  }
-  if (isset($_GET['applicationID'])) {
-    $appid = $_GET['applicationID'];
-  }
-  if (isset($_GET['fname'])) {
-    $first = $_GET['fname'];
-  }
-  if (isset($_GET['lname'])) {
-    $last = $_GET['lname'];
-  }
-  //define("appliID", $appid);
-
-  $myReview = $myComment = "";
-  $myReviewErr = $myCommentErr = "";
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(empty($_POST['recommendation'])){
-      $myReviewErr = "Please make a recommendation";
-    }
-    else{
-      $myReview = $_POST['recommendation'];
-    }
-
-    if(strlen($_POST['comment']) > 50){
-      $myCommentErr = "Exceeded character limit of 50";
-    }
-    elseif(empty($_POST['comment'])){
-      $myCommentErr = "Comment can't be empty";
-    }
-    else{
-      $myComment = $_POST['comment'];
-    }
-  }
-  //
-  // -------------------------------------------------------------------------------
-
-
   
+  $first = '';
+  $last = '';
+  $id = '';
+	
+	include ('php/connectvars.php');
+  
+  if (isset($_POST['review'])) {
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    
+    $id = $_POST['id'];
+
+    $query = 'SELECT fname, lname FROM applicant WHERE username='.$_POST['id'];
+    $res = mysqli_query($dbc,$query);
+
+    if(mysqli_num_rows($res)) {
+      $row = mysqli_fetch_array($res);
+      
+      $first = $row['fname'];
+      $last = $row['lname'];
+    }
+  }
+
+  if (isset($_POST['submit'])) {
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $sql = "UPDATE application SET reviewer_comment ='".$_POST['comment']."' WHERE username=".$_POST['id'];
+    $res = mysqli_query($dbc,$sql);
+
+    $sql = "UPDATE application SET recommendation =".$_POST['recommendation']." WHERE username=".$_POST['id'];
+    $res = mysqli_query($dbc,$sql);
+  }
 ?>
 
 <body>
@@ -85,22 +54,12 @@
     <form method="post" class="card p-5 mt-4" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
       <div class="row">
           <div class="col-md-6 form-group">
-              <label for="fname">Name</label>                            
+              <label for="fname">Name: </label>                            
               <?php echo $first?> <?php echo $last?>
           </div>
           <div class="col-md-6 form-group">
-              <label for="fname">Application ID</label>                            
-              <?php echo $appid?>
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-md-6 form-group">
-              <label for="mathscore">GRE Score - Math</label>                            
-              <?php echo $greMath?>
-          </div>
-          <div class="col-md-6 form-group">
-              <label for="englishscore">GRE Score - English</label>                            
-              <?php echo $greEnglish?>
+              <label for="fname">Applicant ID: </label>                            
+              <?php echo $id?>
           </div>
       </div>
 
@@ -114,30 +73,28 @@
       <div class="row">
 
           <div class="col form-group text-danger text-center">
-            Reject <input id="recommendation" name="recommendation" type="radio" value="reject" />
+            Reject <input id="recommendation" name="recommendation" type="radio" value="1" required/>
           </div>
 
           <div class="col form-group text-info text-center">
-            Borderline <input id="recommendation" name="recommendation" type="radio" value="borderline" />
+            Borderline <input id="recommendation" name="recommendation" type="radio" value="2" required/>
           </div>
 
           <div class="col form-group text-warning text-center">
-            Accept Without Aid <input id="recommendation" name="recommendation" type="radio" value="withoutaid" />
+            Accept Without Aid <input id="recommendation" name="recommendation" type="radio" value="3" required/>
           </div>
 
           <div class="col form-group text-success text-center">
-            Accept With Aid <input id="recommendation" name="recommendation" type="radio" value="withaid" />
+            Accept With Aid <input id="recommendation" name="recommendation" type="radio" value="4" required/>
           </div>
-          
-          <span class="error"> <?php echo $myReviewErr;?></span><br/><br/>
+          <input type='hidden' name='id' value='<?php echo $id;?>'>
 
       </div>
 
         <div class="row">
           <div class="col-lg form-group">
             <label for="comment">Add Comment</label>
-            <input type="text" class="form-control form-control-lg text-muted" id="comment" name="comment" size="32"/>
-            <span class="error"> <?php echo $myCommentErr;?></span>
+            <input type="text" class="form-control form-control-lg text-muted" maxlength="254" id="comment" name="comment" size="32" required/>
           </div>
       </div>
 
@@ -148,40 +105,5 @@
      </div>
   </form>
   </div>
-
-<?php
-  
-  
-  if(!empty($myReview) && !empty($myComment)){
-    
-    $sql = "UPDATE application SET reviewer_comment = '$myComment' WHERE applicationID = $appid";
-    $x = 0;
-    $sql1 = "";
-    if($dbc->query($sql) == true){
-      if($myReview == "reject"){
-        $x = 1;
-        $sql1 = "UPDATE application SET recommendation = 1 WHERE applicationID = $appid";
-      }
-      elseif($myReview == "borderline"){
-        $x = 2;
-        $sql1 = "UPDATE application SET recommendation = 2 WHERE applicationID = $appid";
-      }
-      elseif($myReview == "withoutaid"){
-        $x = 3;
-        $sql1 = "UPDATE application SET recommendation = 3 WHERE applicationID = $appid";
-      }
-      else{
-        $x = 4;
-        $sql1 = "UPDATE application SET recommendation = 4 WHERE applicationID = $appid";
-      }
-      
-     
-      if($dbc->query($sql1) == true){
-      echo '<br/> Thank you, you recommendation has been recorded <br/>';
-      }
-    }
-  }
-
-?>  
 </body>
 </html>
--- Farm-Fresher/src/reviewer_portal.php
+++ Farm-Fresher/src/reviewer_portal.php
@@ -8,6 +8,9 @@
 	?>
 </head>
 
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+<br/><br/><br/><br/><br/>
+<b>Current Applications for Review:</b><br />
 <?php
 
   if (empty($_SESSION['id'])) {
@@ -21,9 +24,20 @@
   $data = mysqli_query($dbc, $appQ);
   if (mysqli_num_rows($data)) {
     while ($row = mysqli_fetch_array($data)) {
-      echo "<p>".$row['applicantid']."</p><br/>";
+      echo "<p>".$row['applicantid']."</p>
+      <form action='application_view.php' method='POST'>
+        <input type='hidden' name='id' value='".$row['applicantid']."' />
+        <input type='submit' id='btn' value='View Application' name='view' class='btn btn-primary btn-lg px-5' />
+      </form>
+      <form action='review_form.php' method='POST'>
+        <input type='hidden' name='id' value='".$row['applicantid']."' />
+        <input type='submit' id='btn' value='Submit Review' name='review' class='btn btn-primary btn-lg px-5' />
+      </form>
+      <br/><br/>";
     }
   }
 
 ?>
+
 </body>
+</html>
