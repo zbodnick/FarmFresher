a33103cb57b72c3859d63eda2b06e10bc7504c14
Ubuntu <ubuntu@ip-172-31-86-174.ec2.internal>
2020-04-29 01:12:44 +0000

    user feedback

--- Farm-Fresher/src/connectvars.php
+++ Farm-Fresher/src/connectvars.php
@@ -1,8 +1,4 @@
 
 <?php
-  // Define database connection constants
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'jakejh'); //replace this
-  define('DB_PASSWORD', 'mindstormA1!'); //edit this
-  define('DB_NAME', 'jakejh'); //edit this
+
 ?>
--- Farm-Fresher/src/header.php
+++ Farm-Fresher/src/header.php
@@ -76,7 +76,10 @@
 								</li>
 								<li>
 								  <a href="grades.php" class="nav-link text-left">Grades</a>
-								</li>
+                </li>
+                <li>
+								  <a href="reviewer_portal.php" class="nav-link text-left">Application Review</a>
+								</li>	
 							';
 							break;
 						case "Student":
@@ -92,9 +95,23 @@
 								</li>
 								<li>
 								  <a href="transcript.php" class="nav-link text-left">Transcript</a>
-								</li>			
+                </li>	
 							';
-							break;
+              break;
+            case "Applicant":
+              echo '
+                <li>
+								  <a href="application_view.php" class="nav-link text-left">View Application</a>
+							  </li>
+              ';
+              break;
+            case "CAC":
+              echo '
+                <li>
+                  <a href="reviewer_portal.php" class="nav-link text-left">Update Applications</a>
+                </li>
+              ';
+              break;
 						case "GS":
 							echo '	
 								<li>
@@ -102,6 +119,9 @@
 								</li>
 								<li>
 								  <a href="transcript.php" class="nav-link text-left">Transcripts</a>
+                </li>
+                <li>
+								  <a href="reviewer_portal.php" class="nav-link text-left">Update Applications</a>
 								</li>
 							';
 							break;
