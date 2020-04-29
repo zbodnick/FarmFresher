37f13aad3ea793ffa462b92276ebb66f2d2c0985
zbodnick <ubuntu@ip-172-31-44-167.ec2.internal>
2020-04-29 00:34:30 +0000

    Added has_hold bool to regs.sql and implemented advising hold on registration

--- Farm-Fresher/src/connectvars.php
+++ Farm-Fresher/src/connectvars.php
@@ -2,7 +2,7 @@
 <?php
   // Define database connection constants
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'jakejh'); //replace this
-  define('DB_PASSWORD', 'mindstormA1!'); //edit this
-  define('DB_NAME', 'jakejh'); //edit this
+  define('DB_USER', 'zbodnick'); //replace this
+  define('DB_PASSWORD', 'FarmFr3$h'); //edit this
+  define('DB_NAME', 'FarmFresher'); //edit this
 ?>
