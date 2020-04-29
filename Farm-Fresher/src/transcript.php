8f0cd339b8025faa1b6dbf04f2bef31da81a5618
Jake Harris <jakejh@gwu.edu>
2020-04-28 21:44:46 -0400

    Transcript GPA at bottom

--- Farm-Fresher/src/transcript.php
+++ Farm-Fresher/src/transcript.php
@@ -204,6 +204,70 @@
 					  </div>';
 			}
 
+      // BELOW IS JAKE's GPA STUFF
+
+      $query = "select DISTINCT u_id, semester, year, grade, title, credits, courses_taken.crn from courses_taken join schedule join catalog WHERE u_id = ". $_SESSION['id'] ." and catalog.c_id = courses_taken.crn;";
+
+      $result= mysqli_query($dbc, $query);
+
+      echo "<center><h4>University ID : ".$_SESSION['id']."</h4></center>";
+
+      if ($result->num_rows > 0)
+        {
+        //IF USER IS AN ALUMNI, SHOW FINAL GPA
+        if(strcmp($_SESSION['uType'], 'alumni') == 0){
+          $query = "select gpa from student where u_id = " . $_SESSION['id'];
+          $result = mysqli_query($dbc, $query);
+          $row = $result->fetch_assoc();
+
+          echo "<br><center><h4>Final GPA : ".$row["gpa"]."</h4></center>";
+        }else{
+          $id = $_SESSION['id'];
+          $queryA = "SELECT SUM(CASE grade WHEN 'A' THEN 1 ELSE 0 END) totalA FROM courses_taken WHERE u_id = $id;";
+          $numberOfAs = mysqli_query($dbc, $queryA);
+          $numberOfAs = $numberOfAs->fetch_assoc();
+          $resultA= $numberOfAs['totalA'];
+
+          $queryB = "SELECT SUM(CASE grade WHEN 'B' THEN 1 ELSE 0 END) totalB FROM courses_taken WHERE u_id = $id;";
+          $numberOfBs = mysqli_query($dbc, $queryB);
+          $numberOfBs = $numberOfBs->fetch_assoc();
+          $resultB= $numberOfBs['totalB'];
+
+          $queryC = "SELECT SUM(CASE grade WHEN 'C' THEN 1 ELSE 0 END) totalC FROM courses_taken WHERE u_id = $id;";
+          $numberOfCs = mysqli_query($dbc, $queryC);
+          $numberOfCs = $numberOfCs->fetch_assoc();
+          $resultC= $numberOfCs['totalC'];
+
+          $queryD = "SELECT SUM(CASE grade WHEN 'D' THEN 1 ELSE 0 END) totalD FROM courses_taken WHERE u_id = $id;";
+          $numberOfDs = mysqli_query($dbc, $queryD);
+          $numberOfDs = $numberOfDs->fetch_assoc();
+          $resultD= $numberOfDs['totalD'];
+
+          $queryF = "SELECT SUM(CASE grade WHEN 'F' THEN 1 ELSE 0 END) totalF FROM courses_taken WHERE u_id = $id;";
+          $numberOfFs = mysqli_query($dbc, $queryF);
+          $numberOfFs = $numberOfFs->fetch_assoc();
+          $resultF= $numberOfFs['totalF'];
+
+          $query2 = "select sum(A.credits) credits from (select DISTINCT u_id, semester, year, grade, title, credits, department from courses_taken join schedule join catalog WHERE u_id = " . $id . " and catalog.c_id = courses_taken.crn and department = 'CSCI') as A;";
+          $chours = mysqli_query($dbc, $query2);
+          $chours = $chours->fetch_assoc();
+          $totalhours= $chours['credits'] + 0.00;
+
+          function avgGPAfunction($resultA, $resultB, $resultC, $resultD, $resultF, $totalhours){
+                $attemptedhours = ($resultA * 4.00 * 3.00) + ($resultB * 3.00 * 3.00) + ($resultC * 2.00 * 3.00) + ($resultD * 1.00 * 3.00) + ($resultF * 0.00 * 3.00);
+                $avggpa = $attemptedhours / $totalhours;
+                return $avggpa;
+          }
+          $avggpa = avgGPAfunction($resultA, $resultB, $resultC, $resultD, $resultF, $totalhours);
+          echo "<br><center><h4>Final GPA : ".$avggpa."</h4></center>";
+
+        }
+      }
+      else
+      {
+        echo "<center>No Results Found</center";
+      }
+
         ?>
 
     </div>
