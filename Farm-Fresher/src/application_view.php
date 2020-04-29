47edce392a94c3dfb93ace6025563e8776f1d924
zbodnick <ubuntu@ip-172-31-44-167.ec2.internal>
2020-04-29 01:26:38 +0000

    made has_hold 1 on applicant -> student matriculaiton insert

--- Farm-Fresher/src/application_view.php
+++ Farm-Fresher/src/application_view.php
@@ -24,7 +24,7 @@ if (isset($_POST['accept'])) {
   $data = mysqli_query($dbc, $query);
   $applicantData = mysqli_fetch_array($data);
 
-  $query = "INSERT INTO student VALUES (".$applicantData['username'].",'".$applicantData['fname']."','".$applicantData['lname']."','".$applicantData['addr']."','".$applicantData['email']."','Computer Science', NULL, NULL, NULL, NULL, NULL)";
+  $query = "INSERT INTO student VALUES (".$applicantData['username'].",'".$applicantData['fname']."','".$applicantData['lname']."','".$applicantData['addr']."','".$applicantData['email']."','Computer Science', NULL, NULL, NULL, NULL, NULL, 1)";
   $data = mysqli_query($dbc, $query);
 
   $query = "UPDATE users SET p_level='Student' WHERE id=".$_SESSION['id'];
