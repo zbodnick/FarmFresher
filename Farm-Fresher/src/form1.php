b67db6c0911f31ddb4ca49c29412d5705a762d07
Jake Harris <jakejh@gwu.edu>
2020-04-28 23:43:12 -0400

    Fix

--- Farm-Fresher/src/form1.php
+++ Farm-Fresher/src/form1.php
@@ -26,7 +26,6 @@ $query = "SELECT * from formone where universityid = $id";
 $result = mysqli_query($dbc, $query);
 
 if(mysqli_num_rows($result) > 0){
-	?><script type="text/javascript">window.alert("Form One has already been submitted.");</script><?php
 	header("Location: home.php");
 	}?>
 	<script type="text/javascript">
